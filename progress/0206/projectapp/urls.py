80865fd161ec9f8ce686cd3b1017d642fd4a297c
jinsujeong <ejd090393@gmail.com>
2021-02-06 20:07:28 +0900

    2021.02.06

---
+++ progress/0206/projectapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('main/', views.main, name = 'main'),
+    path('map1/', views.map1, name = 'map1'),
+    path('map2/', views.map2, name = 'map2'),
+    path('board/', views.board, name = 'board'),
+    path('register/', views.register, name= 'register'), # 수정's 추가
+    path('login/', views.login, name= 'login'), # 수정's 추가
+]
